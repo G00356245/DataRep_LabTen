841827a5900d7d6c889ab9fff503eb682f2730e8
Ray Htoo <g00356245>
2020-10-31 21:51:10 +0000

    lab4 completed

--- src/components/create.js
+++ src/components/create.js
@@ -3,13 +3,89 @@ import '../App.css';
 
 //Create class
 export class Create extends React.Component {
+    //added constructor.
+    constructor() {
+        super();   //this line of code is needed to use the form.
+
+        //binding the methods so that it can execute when called.
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeYear = this.onChangeYear.bind(this);
+        this.onChangePoster = this.onChangePoster.bind(this);
+
+        //stating the type of data that the users will input.
+        this.state = {
+            Title: '',
+            Year: '',
+            Poster: ''
+        }
+    }
+
+    //method for calling the onchangeYear.
+    onChangeYear(e) {
+        this.setState({
+            Year: e.target.value
+        });
+    }
+
+    //method for calling the onchangePoster. 
+    onChangePoster(e) {
+        this.setState({
+            Poster: e.target.value
+        });
+    }
+
+    //method for calling the onchangetitle. 
+    onChangeTitle(e) {
+        this.setState({
+            Title: e.target.value
+        });
+    }
+
+    //method for onsubmit
+    onSubmit(e) {
+        e.preventDefault(); //stop the button from  being called multiple times.
+        alert("Movie: " + this.state.Title + " " + this.state.Year + " " + this.state.Poster);  //this line will alert whenever the submit button is clicked.
+    }
 
     render() {
+        //adding forms for movie title. year and poster to the create page to be able to input data.
+        //onSubmit allows for the submission of input data in the form.
+        //added a submit button at the end so that the users is able to submit data by clicking the button.
         return (
-            //dive tag to display text on website.
-            <div>
-                <h1>This is my Create component.</h1>
+            <div className='App'>
+                <form onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label>Add Movies Title: </label>
+                        <input type='text'
+                            className='form-control'
+                            value={this.state.Title}
+                            onChange={this.onChangeTitle}></input>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Add Movie Year: </label>
+                        <input type='text'
+                            className='form-control'
+                            value={this.state.year}
+                            onChange={this.onChangeYear}></input>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Movies Poster: </label>
+                        <textarea type='text'
+                            className='form-control'
+                            value={this.state.Poster}
+                            onChange={this.onChangePoster}></textarea>
+                    </div>
+
+                    <div className='form-group'>
+                        <input type='submit'
+                            value='Add Movie'
+                            className='btn btn-primary'></input>
+                    </div>
+                </form>
             </div>
         );
     }
-}
+} 
--- src/components/read.js
+++ src/components/read.js
@@ -1,37 +1,27 @@
 import React from 'react';
 import { Movies } from './movies';
+import axios from 'axios';//imported axios 
 
 //Read class
 export class Read extends React.Component {
 
-    //state will hold the data that we want to import.
-    //stating the list of movies that we want to display.
+    //method to call list of movies.
     state = {
-        movies: [
-            {
-                "Title": "Avengers: Infinity War",
-                "Year": "2018",
-                "imdbID": "tt4154756",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Captain America: Civil War",
-                "Year": "2016",
-                "imdbID": "tt3498820",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Charlie Wilson's War",
-                "Year": "2007",
-                "imdbID": "tt0472062",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-        ]
+        movies: []
 
     };
+
+    //using a life cyclehook, this method will get called everytime our componeent is mounted.
+    componentDidMount() {
+        axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+            .then((response) => {
+                this.setState({ movies: response.data.Search })
+            })    //this will use the fufill state.
+            .catch(
+                (error) => {
+                    console.log(error)
+                });  //if the .then function doesn't work then this line will run.
+    }
 
     render() {
         return (
